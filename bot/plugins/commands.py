c13f6fcce254d7b4cbd3be01a48fb384bb9be588
Saranhgg <87320099+Saranhgg@users.noreply.github.com>
2021-07-20 21:29:32 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -28,7 +28,7 @@ async def start(bot, update):
             await update.reply_cached_media(
                 file_id,
                 quote=True,
-                caption = join now👉 @on_air_movies ,
+                caption = join now 👇👇,
                 parse_mode="html",
                 reply_markup=InlineKeyboardMarkup(
                     [
