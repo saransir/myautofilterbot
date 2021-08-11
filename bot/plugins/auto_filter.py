b63871f895ecde74f664fea0e90fad34dfb49d45
Saranhgg <87320099+Saranhgg@users.noreply.github.com>
2021-08-11 13:27:07 +0530

    Update auto_filter.py

--- bot/plugins/auto_filter.py
+++ bot/plugins/auto_filter.py
@@ -81,7 +81,7 @@ async def auto_filter(bot, update):
             file_size = "" if file_size == ("[0 B]") else file_size
             
             # add emoji down below inside " " if you want..
-            button_text = f"👉{file_size}👉{file_name}"
+            button_text = f"{file_size}⭕️{file_name}"
             
 
             if file_type == "video":
@@ -153,7 +153,7 @@ async def auto_filter(bot, update):
         
         # Just A Decaration
         result[0].append([
-            InlineKeyboardButton(f"🇲🇰 Page 1/{len_result if len_result < max_pages else max_pages} 🇲🇰", callback_data="ignore")
+            InlineKeyboardButton(f"⭕️ Page 1/{len_result if len_result < max_pages else max_pages} ⭕️", callback_data="ignore")
         ])
         
         
@@ -203,7 +203,7 @@ async def auto_filter(bot, update):
         try:
             await bot.send_message(
                 chat_id = update.chat.id,
-                text=f"Found {(len_results)} 🔍result: <code>{query}</code>",
+                text=f"Found {(len_results)} 🔍result: <code>{query}</code>       👇👇",
                 reply_markup=reply_markup,
                 parse_mode="html",
                 reply_to_message_id=update.message_id
