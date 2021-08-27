3cc415354a784e5b40d5d0171ab89dd6bb09037d
Saranhgg <87320099+Saranhgg@users.noreply.github.com>
2021-08-27 22:37:20 +0530

    Update auto_filter.py

--- bot/plugins/auto_filter.py
+++ bot/plugins/auto_filter.py
@@ -203,7 +203,7 @@ async def auto_filter(bot, update):
         try:
             await bot.send_message(
                 chat_id = update.chat.id,
-                text=f"Found {(len_results)} 🔍result: <code>{query}</code>       👇👇",
+                text=f"<b>⭕️Requested Movie: <code>{query}</code></b>\n⭕️Results Found: {(len_results)}\n channel 👉 @on_air_movies",
                 reply_markup=reply_markup,
                 parse_mode="html",
                 reply_to_message_id=update.message_id
