e6a26540734a66ddc5cbb9d06c9f994f6b8775a0
Saranhgg <87320099+Saranhgg@users.noreply.github.com>
2021-08-28 16:54:18 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -23,7 +23,7 @@ async def start(bot, update):
         if (file_id or file_type) == None:
             return
         
-        caption = file_caption if file_caption != ("" or None) else ("<code>" + file_name + "</code>"<b>\n\n⭕️𝙲𝚑𝚊𝚗𝚗𝚎𝚕-- @on_air_movies ⭕️\n👇𝙼𝚘𝚟𝚒𝚎 searching 𝚐𝚛𝚘𝚞𝚙👇</b>)
+        caption = ("<code>" + file_name + "</code>"<b>\n\n⭕️𝙲𝚑𝚊𝚗𝚗𝚎𝚕-- @on_air_movies ⭕️\n👇𝙼𝚘𝚟𝚒𝚎 searching 𝚐𝚛𝚘𝚞𝚙👇</b>)
         try:
             await update.reply_cached_media(
                 file_id,
