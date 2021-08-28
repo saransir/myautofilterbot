a04bc7624c7d4b8f86e56555b21acbcc4738c3e2
Saranhgg <87320099+Saranhgg@users.noreply.github.com>
2021-08-28 09:19:19 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -28,7 +28,7 @@ async def start(bot, update):
             await update.reply_cached_media(
                 file_id,
                 quote=True,
-                caption = "<b>file_name\n\n⭕️𝙲𝚑𝚊𝚗𝚗𝚎𝚕-- @on_air_movies ⭕️\n👇𝙼𝚘𝚟𝚒𝚎 searching 𝚐𝚛𝚘𝚞𝚙👇</b> ",
+                caption =(file_name)"<b>⭕️𝙲𝚑𝚊𝚗𝚗𝚎𝚕-- @on_air_movies ⭕️\n👇𝙼𝚘𝚟𝚒𝚎 searching 𝚐𝚛𝚘𝚞𝚙👇</b> ",
                 parse_mode="html",
                 reply_markup=InlineKeyboardMarkup(
                     [
